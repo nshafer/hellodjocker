41d2df78f85d8e8957c73bff684b268900da1177
Nathan Shafer <nate@lotech.org>
2016-03-07 17:26:51 -0700

    Added signal handling to make runserver stop when docker sends SIGTERM. Also, stop on SIGINT sent to parent (like from PyCharm)

--- manage.py
+++ manage.py
@@ -1,8 +1,16 @@
 #!/usr/bin/env python
 import os
 import sys
+import signal
+
+
+def sighandler(signum, frame):
+    print("signal {} received, quitting".format(signum))
+    sys.exit(1)
 
 if __name__ == "__main__":
+    signal.signal(signal.SIGTERM, sighandler)
+    signal.signal(signal.SIGINT, sighandler)
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "hellodjocker.settings")
 
     from django.core.management import execute_from_command_line
