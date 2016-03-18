5dda6c7cc56af5ad6c63f056f095440b41029953
Nathan Shafer <nate@lotech.org>
2016-03-18 11:56:01 -0700

    Added migrate

--- app/cmd.production.sh
+++ app/cmd.production.sh
@@ -6,5 +6,8 @@ set -x
 # Make sure that the static volume is updated if the image has been rebuilt
 python manage.py collectstatic --noinput
 
+# Migrate any database changes
+python manage.py migrate
+
 # Exec so that gunicorn replaces this process and gets all of the signals from Docker
 exec gunicorn hellodjocker.wsgi --bind 0.0.0.0:9001 --workers 2
